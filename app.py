d5041557b390de898539886e26483fe0dcd5ed85
velagamouli18 <167733988+velagamouli18@users.noreply.github.com>
2026-06-16 16:26:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import requests
 from groq import Groq
 from dotenv import load_dotenv
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.docstore.document import Document
